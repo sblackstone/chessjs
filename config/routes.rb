55be4d812c76ea828102d553b943fba72551c5dd
Stephen Blackstone <sblackstone@gmail.com>
2016-08-19 20:26:54 -0400

    nevermind, back to teaspoon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   get 'home/index'
-  
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
